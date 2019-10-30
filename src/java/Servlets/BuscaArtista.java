c91c4e858ce1a1daa46e2fe058146a4222beaa5e
Tuliolivieri <37648167+Tuliolivieri@users.noreply.github.com>
2019-10-30 15:14:49 -0300

    Add files via upload

--- src/java/Servlets/BuscaArtista.java
+++ src/java/Servlets/BuscaArtista.java
@@ -5,17 +5,33 @@
  */
 package Servlets;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.StringReader;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 /**
  *
  * @author Aluno
+ * 
+ * //// CONSULTA CANTOR
+ * http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=Name&api_key=917a49b4c9325caecc0bec862aae4397
+ * 
+ * /// CONSULTA TOP 10
+ * http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&limit=10&api_key=917a49b4c9325caecc0bec862aae4397
  */
 @WebServlet(name = "BuscaArtista", urlPatterns = {"/BuscaArtista"})
 public class BuscaArtista extends HttpServlet {
@@ -28,9 +44,33 @@ public class BuscaArtista extends HttpServlet {
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
+     * @throws javax.xml.parsers.ParserConfigurationException
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException
+    {
+        DocumentBuilderFactory factory;
+        DocumentBuilder builder;
+        Document doc = null;
+
+        String artista = request.getParameter("nomeArtista");
+        String consulta = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + artista + "&api_key=917a49b4c9325caecc0bec862aae4397";
+        try 
+        {
+            factory=DocumentBuilderFactory.newInstance();
+            builder = factory.newDocumentBuilder();
+            doc = (Document) builder.parse(new InputSource(new StringReader(consulta)));
+            System.out.println("MFOEE");
+            System.out.println(doc.getText(0, 100));
+        } 
+        catch (ParserConfigurationException | BadLocationException| SAXException e) 
+        {
+            System.out.println("Erro: " + e.getMessage());
+        }
+        
+        
+        //System.out.println("");
+        
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
@@ -41,6 +81,8 @@ public class BuscaArtista extends HttpServlet {
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet BuscaArtista at " + request.getContextPath() + "</h1>");
+            out.println("<h4>" + artista + "</h4>");
+            out.println("<h>" + consulta + "</h>");
             out.println("</body>");
             out.println("</html>");
         }
