ae68c39eabc8dff1c5a7d6c5dd6d9fee951af11e
Tuliolivieri <37648167+Tuliolivieri@users.noreply.github.com>
2019-11-03 17:16:39 -0300

    Add files via upload

--- src/java/Servlets/BuscaArtista.java
+++ src/java/Servlets/BuscaArtista.java
@@ -21,12 +21,14 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 /**
  *
- * @author Aluno
+ * @author Tulio
  * 
  * //// CONSULTA CANTOR
  * http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=Name&api_key=917a49b4c9325caecc0bec862aae4397
@@ -48,46 +50,113 @@ public class BuscaArtista extends HttpServlet {
      * @throws javax.xml.parsers.ParserConfigurationException
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException
-    {
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String artista;
         DocumentBuilderFactory factory;
         DocumentBuilder builder;
         Document doc = null;
-
-        String artista = request.getParameter("nomeArtista");
-        String consulta = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + artista + "&api_key=917a49b4c9325caecc0bec862aae4397";
-        try 
+        System.out.println(request.getRequestURI());
+        artista = request.getParameter("nomeArtista");
+        if(artista != null && !artista.equals(""))
         {
-            factory=DocumentBuilderFactory.newInstance();
-            builder = factory.newDocumentBuilder();
-            //doc = builder.parse(consulta);
-            doc=builder.parse(new InputSource(consulta));
-
-            System.out.println(doc.getTextContent());
-        } 
-        catch (ParserConfigurationException | SAXException e) 
+            artista = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + artista + "&api_key=917a49b4c9325caecc0bec862aae4397";
+            try
+            {
+                factory = DocumentBuilderFactory.newInstance();
+                builder = factory.newDocumentBuilder();
+                doc = builder.parse(artista);
+                
+                artista = "<ul style=\"color: white; padding-left:10vw; padding-right:10vw; padding-top:5vh; font-family: Calibri, serif; font-size: 24px;\">";
+                
+                NodeList nodes = doc.getElementsByTagName("name");
+                Element element = (Element) nodes.item(0);
+                artista += "<ol style=\"font-size: 32px\">" + element.getTextContent() + "</ol>";
+                
+                nodes = doc.getElementsByTagName("listeners");
+                element = (Element) nodes.item(0);
+                artista += "<ol style=\"padding-top: 15px\"> Ouvintes: " + element.getTextContent() + "</ol>";
+                
+                nodes = doc.getElementsByTagName("playcount");
+                element = (Element) nodes.item(0);
+                artista += "<ol> Vezes tocado: " + element.getTextContent() + "</ol>";
+                
+                nodes = doc.getElementsByTagName("summary");
+                for (int i = 0; i < nodes.getLength(); i++) 
+                {
+                  element = (Element) nodes.item(i);
+                  artista += "<ol style=\"padding-top: 15px\">" + element.getTextContent() + "</ol>";
+                }
+                 artista += "</ul>";
+                 
+                String resp = "<html>\n" +
+                        "    <head>\n"
+                        + "        <title>Artistas Web</title>\n"
+                        + "        <meta charset=\"UTF-8\">\n"
+                        + "        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n"
+                        + "        <link href=\"estilo.css\" rel=\"stylesheet\" type=\"text/css\"/>\n"
+                        + "    </head>\n"
+                        + "    <body>\n"
+                        + "        <header>\n"
+                        + "            Info Artistas\n"
+                        + "        </header>\n"
+                        + "        <main >\n"
+                        + artista            
+                        + "        </main>\n"
+                        + "    </body>\n"
+                        + "</html>";
+                        
+                response.getWriter().print(resp);
+            }
+            catch(Exception e)
+            {
+                System.out.println("Artista" + e.toString());
+            }
+        }
+        else
         {
-            System.out.println("Erro: " + e.getMessage());
+            artista = "http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&limit=10&api_key=917a49b4c9325caecc0bec862aae4397";
+            System.out.println("TOP 100");
+            try
+            {
+                factory = DocumentBuilderFactory.newInstance();
+                builder = factory.newDocumentBuilder();
+                doc = builder.parse(artista);
+                artista = "<ul style=\"color: white; padding-top:30px; font-family: Calibri, serif; font-size: 24px;\">";
+                NodeList nodes = doc.getElementsByTagName("name");
+                for (int i = 0; i < nodes.getLength(); i++) 
+                {
+                  Element element = (Element) nodes.item(i);
+                  artista += "<ol>" +  (i + 1) + "º - " + element.getTextContent() + "</ol>";
+                }
+                 artista += "</ul>";
+                 
+                 String resp = "<html>\n" +
+                        "    <head>\n"
+                        + "        <title>Artistas Web</title>\n"
+                        + "        <meta charset=\"UTF-8\">\n"
+                        + "        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n"
+                        + "        <link href=\"estilo.css\" rel=\"stylesheet\" type=\"text/css\"/>\n"
+                        + "    </head>\n"
+                        + "    <body>\n"
+                        + "        <header>\n"
+                        + "            Info Artistas\n"
+                        + "        </header>\n"
+                        + "        <main>\n"
+                        + "<h4 style=\"font-family: Calibri, serif; font-weight: normal; font-size: 36px; color: white; margin-top: 30px;\">Top 10 Artistas</h4>"
+                        + artista            
+                        + "        </main>\n"
+                        + "    </body>\n"
+                        + "</html>";
+                 
+                response.getWriter().print(resp);
+            }
+            catch(Exception e)
+            {
+                System.out.println("Top 100" + e.toString());
+            }
         }
         
-        
-        //System.out.println("");
-        
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet BuscaArtista</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet BuscaArtista at " + request.getContextPath() + "</h1>");
-            out.println("<h4>" + artista + "</h4>");
-            out.println("<h>" + consulta + "</h>");
-            out.println("</body>");
-            out.println("</html>");
-        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
